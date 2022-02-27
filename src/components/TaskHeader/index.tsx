f21fe4631d6d9847be0ed86f77094ab63a9904e0
takeyu1013 <yuto1013@gmail.com>
2022-02-27 12:36:47 +0900

    💄 文字の色を変更

--- src/components/TaskHeader/index.tsx
+++ src/components/TaskHeader/index.tsx
@@ -3,7 +3,7 @@ import type { VFC } from "react";
 type Props = {};
 
 const TaskHeader: VFC<Props> = () => {
-  return <h1>今日する</h1>;
+  return <h1 className="text-rose-500">今日する</h1>;
 };
 
 export default TaskHeader;
