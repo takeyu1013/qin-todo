4b3a49d90b19c7ce76979b1c85b0fcf8140e0eab
takeyu1013 <yuto1013@gmail.com>
2022-03-12 11:35:13 +0900

    ⚗️ コンポーネントを試験的に表示するページを追加

---
+++ src/pages/components/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { TaskInput } from "../../components/TaskInput";
+
+const Components: NextPage = () => {
+  return (
+    <>
+      <TaskInput />
+    </>
+  );
+};
+
+export default Components;
