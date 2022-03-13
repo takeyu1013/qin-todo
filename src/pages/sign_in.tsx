0a88add35509be4534ee597588d9dd3f0f0e79a7
Natch <17216462+Natch@users.noreply.github.com>
2022-03-14 00:00:18 +0900

    認証処理を SignInPage にラップするのではなく、 HomePage をラップするように

--- src/pages/sign_in.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import { SignIn } from "../components/SignIn";
-import { supabase } from "../../utils/supabaseClient";
-
-const SignInPage: NextPage = () => {
-  return <SignIn supabaseClient={supabase}></SignIn>;
-};
-
-export default SignInPage;
