7a69430ba1d0181afce53f5fb1168b2372304923
arai motoki <arai@nal.vn>
2022-03-14 00:47:53 +0900

    ✨ Headerコンポーネントを追加

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type HeaderProps = {
+  user: {
+    image: string;
+  };
+};
+
+export const Header = (props: HeaderProps) => {
+  return (
+    <header className="flex justify-between items-center px-[196px] h-20 w-full bg-white">
+      <Link href="/">
+        <a>
+          <Image
+            src="/QinToDo.svg"
+            alt="Qintodoサムネイル"
+            width="112px"
+            height="24px"
+          />
+        </a>
+      </Link>
+      <Link href="#">
+        <a>
+          <Image
+            src={props.user.image}
+            alt="プロフィール画像"
+            width="36px"
+            height="36x"
+          />
+        </a>
+      </Link>
+    </header>
+  );
+};
