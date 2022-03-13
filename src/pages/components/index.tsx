07845578f9ed3d10d1b838f5f79286e98373d7d8
takeyu1013 <67428664+takeyu1013@users.noreply.github.com>
2022-03-13 20:55:04 +0900

    🚚 コンポーネント名を変更

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
-const Components: NextPage = () => {
+const ComponentsPage: NextPage = () => {
   return <></>;
 };
 
-export default Components;
+export default ComponentsPage;
