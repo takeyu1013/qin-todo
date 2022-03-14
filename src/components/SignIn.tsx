3bec6cfdbb0ce60df6445dab26042bd1f9127551
Natch <17216462+Natch@users.noreply.github.com>
2022-03-14 23:28:36 +0900

    SignIn Component を特化仕様から汎用仕様に変更した

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,19 +1,14 @@
-import { VFC } from "react";
+import { ReactNode, VFC } from "react";
 import { Auth } from "@supabase/ui";
 import { SignInPresenter } from "./SignInPresenter";
-import { TaskHeader } from "./TaskHeader";
 import { supabase } from "../../utils/supabaseClient";
 
-export const SignIn: VFC = () => {
+type Props = {
+  children: ReactNode;
+};
+
+export const SignIn: VFC<Props> = ({ children }) => {
   const { user } = Auth.useUser();
 
-  return (
-    <>
-      {user ? (
-        <TaskHeader schedule={"today"}></TaskHeader>
-      ) : (
-        <SignInPresenter supabaseClient={supabase} />
-      )}
-    </>
-  );
+  return <>{user ? children : <SignInPresenter supabaseClient={supabase} />}</>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import { SignIn } from "../components/SignIn";
+import { TaskHeader } from "../components/TaskHeader";
 
 const Home: NextPage = () => {
-  return <SignIn />;
+  return (
+    <SignIn>
+      <TaskHeader schedule={"today"} />
+    </SignIn>
+  );
 };
 
 export default Home;
