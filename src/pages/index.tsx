208cb859053dd00e9ebaaba506100aff18c94a83
Natch <17216462+Natch@users.noreply.github.com>
2022-03-14 23:15:01 +0900

    不要なpropsの受け渡しを除去

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from "next";
-import { supabase } from "../../utils/supabaseClient";
 import { SignIn } from "../components/SignIn";
 
 const Home: NextPage = () => {
-  return <SignIn supabaseClient={supabase}></SignIn>;
+  return <SignIn />;
 };
 
 export default Home;
