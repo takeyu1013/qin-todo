7cfbc666d853d17fd105ef7ab66c042f636dded8
Natch <17216462+Natch@users.noreply.github.com>
2022-03-14 23:14:43 +0900

    Auth.UserContextProvider を app のレイヤーでラップするように修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Auth } from "@supabase/ui";
+import { supabase } from "../../utils/supabaseClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </Auth.UserContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
