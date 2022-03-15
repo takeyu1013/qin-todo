91082b3b24b53543d3137375a0d515a3226e05a7
Natch <17216462+Natch@users.noreply.github.com>
2022-03-15 12:43:41 +0900

    セッションがある場合にクライアントとサーバでレンダリングするHTMLが異なり、警告される問題を解決

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, VFC } from "react";
+import { ReactNode, useEffect, useState, VFC } from "react";
+import { User } from "@supabase/supabase-js";
 import { Auth } from "@supabase/ui";
 import { AuthenticationPresenter } from "./AuthenticationPresenter";
 import { supabase } from "../../utils/supabaseClient";
@@ -8,7 +9,17 @@ type Props = {
 };
 
 export const Authentication: VFC<Props> = ({ children }) => {
-  const { user } = Auth.useUser();
+  const { session } = Auth.useUser();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (!user && session) {
+      (async () => {
+        const response = await supabase.auth.api.getUser(session.access_token);
+        setUser(response.user);
+      })();
+    }
+  }, [user, session]);
 
   return (
     <>
