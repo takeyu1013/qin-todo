5ca683cbf8541e0ccec45b4f78192d21fd431229
Natch <17216462+Natch@users.noreply.github.com>
2022-03-17 00:50:54 +0900

    認証コンポーネントの読込中に Loading を表示するように修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 import type { AuthenticationProps } from "../components/Authentication";
 import dynamic from "next/dynamic";
 import { TaskHeader } from "../components/TaskHeader";
+import { Loader } from "../components/Loader";
 
 const DynamicAuthentication = dynamic<AuthenticationProps>(
   () =>
     import("../components/Authentication").then((mod) => mod.Authentication),
   {
     ssr: false,
+    loading: () => <Loader />,
   }
 );
 
