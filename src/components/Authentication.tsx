7d8a1e9e8fac7985c53bd01c50f312fe6c4b9d4c
Natch <17216462+Natch@users.noreply.github.com>
2022-03-19 12:42:42 +0900

    user情報取得処理にエラーハンドリングを追加

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useEffect, useState, VFC } from "react";
-import { User } from "@supabase/supabase-js";
+import { Session, User, ApiError } from "@supabase/gotrue-js";
 import { Auth } from "@supabase/ui";
 import { AuthenticationPresenter } from "./AuthenticationPresenter";
 import { supabase } from "../../utils/supabaseClient";
@@ -13,17 +13,30 @@ export const Authentication: VFC<AuthenticationProps> = ({ children }) => {
   const { session } = Auth.useUser();
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<ApiError | null>(null);
+
+  const getUserBySession = async (session: Session) => {
+    const { user, error } = await supabase.auth.api.getUser(
+      session.access_token
+    );
+    if (error) {
+      setError(error);
+      console.error(error.message, error.status);
+    }
+    if (user) setUser(user);
+  };
 
   useEffect(() => {
     if (!user && session) {
-      (async () => {
-        const response = await supabase.auth.api.getUser(session.access_token);
-        setUser(response.user);
-      })();
+      getUserBySession(session);
     } else {
       setIsLoading(false);
     }
   }, [user, session]);
+
+  if (error) {
+    return <Loader />; // TODO: should be replaced with `Error Boundary`
+  }
 
   if (isLoading) {
     return <Loader />;
