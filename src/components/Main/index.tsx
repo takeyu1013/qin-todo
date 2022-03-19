1dcdba0ebfaf1a61f6199ba8f3e540ad8ddc4156
takeyu1013 <yuto1013@gmail.com>
2022-03-19 22:08:16 +0900

    💄 タスクリスト用のレイアウトを調整

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,35 +1,8 @@
+import Image from "next/image";
 import type { VFC } from "react";
 
 import type { TaskHeaderProps } from "../TaskHeader";
 import { SCHEDULE_LIST, TaskHeader } from "../TaskHeader";
-
-const PlusIcon: VFC = () => {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        cx="12"
-        cy="12"
-        r="11"
-        fill="#C2C6D2"
-        stroke="#C2C6D2"
-        strokeWidth="2"
-      />
-      <path
-        d="M12 12H6.5M12 6.5V12V6.5ZM12 12V17.5V12ZM12 12H17.5H12Z"
-        stroke="white"
-        strokeWidth="1.83333"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
 
 const TaskCard: VFC<TaskHeaderProps> = ({ schedule }) => {
   return (
@@ -37,10 +10,40 @@ const TaskCard: VFC<TaskHeaderProps> = ({ schedule }) => {
       <div className="pb-4">
         <TaskHeader schedule={schedule} />
       </div>
-      <div className="flex">
-        <div className="pr-3">
-          {/* TODO チェックボックスコンポーネントに置き換える */}
-          <PlusIcon />
+      {/* TODO タスクリストのコンポーネントに置き換える */}
+      <ul>
+        <li>
+          <div className="flex py-2">
+            <div className="w-9 h-6 pr-3">
+              <Image
+                src="/EmptyCheckboxIcon.svg"
+                alt="O"
+                width="24px"
+                height="24px"
+              />
+            </div>
+            <p className="text-[#757575] line-through">Next.jsのインストール</p>
+          </div>
+        </li>
+        <li>
+          <div className="flex py-2">
+            <div className="w-9 h-6 pr-3">
+              <Image
+                src="/EmptyCheckboxIcon.svg"
+                alt="O"
+                width="24px"
+                height="24px"
+              />
+            </div>
+            <p>ESLintのインストール</p>
+          </div>
+        </li>
+      </ul>
+      {/* TODO 新規タスクのコンポーネントに置き換える */}
+      <div className="flex py-2">
+        {/* TODO チェックボックスコンポーネントに置き換える */}
+        <div className="w-9 h-6 pr-3">
+          <Image src="/PlusIcon.svg" alt="+" width="24px" height="24px" />
         </div>
         {/* TODO 入力エリアのコンポーネントに置き換える */}
         <input className="outline-none" placeholder="タスクを追加する" />
