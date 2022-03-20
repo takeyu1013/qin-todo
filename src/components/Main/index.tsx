2c98404dc78c5ddb05f1f859f78edd1ebe5ca037
takeyu1013 <yuto1013@gmail.com>
2022-03-20 23:29:31 +0900

    🎨 冗長なコンポーネントを削除

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,9 +3,9 @@ import type { VFC } from "react";
 import { TaskCard } from "../TaskCard";
 import { SCHEDULE_LIST } from "../TaskHeader";
 
-const TaskCardList: VFC = () => {
+export const Main: VFC = () => {
   return (
-    <div className="flex">
+    <main className="px-20 pt-10 flex">
       {Object.values(SCHEDULE_LIST).map((schedule) => {
         return (
           <div className="w-1/3" key={schedule}>
@@ -13,14 +13,6 @@ const TaskCardList: VFC = () => {
           </div>
         );
       })}
-    </div>
-  );
-};
-
-export const Main: VFC = () => {
-  return (
-    <main className="px-20 pt-10">
-      <TaskCardList />
     </main>
   );
 };
