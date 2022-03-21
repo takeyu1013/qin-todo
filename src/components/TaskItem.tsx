eab8ad029dd482a58070c61a1b3263965c48cb86
Natch <17216462+Natch@users.noreply.github.com>
2022-03-21 17:37:22 +0900

    タスクアイテムに複製ボタン・削除ボタンを表示するようにした

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
 import { ChangeEventHandler, VFC } from "react";
+import Image from "next/image";
 
 export type Task = {
   id: number;
@@ -13,14 +14,32 @@ type TaskItemProps = {
 
 export const TaskItem: VFC<TaskItemProps> = ({ task, toggleIsDone }) => {
   return (
-    <label className="flex items-center gap-x-2">
-      <input
-        type="checkbox"
-        value={task.id}
-        checked={task.isDone}
-        onChange={toggleIsDone}
+    <div className="flex items-center gap-x-2">
+      <label className="flex items-center gap-x-2 mr-auto">
+        <input
+          type="checkbox"
+          value={task.id}
+          checked={task.isDone}
+          onChange={toggleIsDone}
+        />
+        {task.isDone ? <del>{task.content}</del> : <span>{task.content}</span>}
+      </label>
+
+      <Image
+        src="/duplicateTask.svg"
+        alt="duplicateTask"
+        width={14.67}
+        height={14.67}
+        objectFit="contain"
       />
-      {task.isDone ? <del>{task.content}</del> : <span>{task.content}</span>}
-    </label>
+
+      <Image
+        src="/deleteTask.svg"
+        alt="deleteTask"
+        width={14.67}
+        height={14.67}
+        objectFit="contain"
+      />
+    </div>
   );
 };
