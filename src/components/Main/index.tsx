2f746eca7d2ac615d2e2c65b4fadb78e631e5b03
takeyu1013 <yuto1013@gmail.com>
2022-03-23 21:32:46 +0900

    💄 TaskCard 間のスペースを gap で調整

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,14 +5,16 @@ import { SCHEDULE_LIST } from "../TaskHeader";
 
 export const Main: VFC = () => {
   return (
-    <main className="h-screen px-20 py-10 flex">
-      {Object.values(SCHEDULE_LIST).map((schedule) => {
-        return (
-          <div className="w-1/3" key={schedule}>
-            <TaskCard schedule={schedule} />
-          </div>
-        );
-      })}
+    <main className="h-screen px-20 py-10 ">
+      <div className="pl-6 flex gap-6">
+        {Object.values(SCHEDULE_LIST).map((schedule) => {
+          return (
+            <div className="w-1/3" key={schedule}>
+              <TaskCard schedule={schedule} />
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 };
