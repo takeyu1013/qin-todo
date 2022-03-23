91fe696dd818037dd8f5c06a1c87c4bc6448ce4f
takeyu1013 <67428664+takeyu1013@users.noreply.github.com>
2022-03-23 21:19:55 +0900

    💄 コンテンツの領域を確保し、画面の下方の余白を追加
    Co-authored-by: Natch <17216462+Natch@users.noreply.github.com>

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,7 +5,7 @@ import { SCHEDULE_LIST } from "../TaskHeader";
 
 export const Main: VFC = () => {
   return (
-    <main className="px-20 pt-10 flex">
+    <main className="h-screen px-20 py-10 flex">
       {Object.values(SCHEDULE_LIST).map((schedule) => {
         return (
           <div className="w-1/3" key={schedule}>
