a5cafc66f8440ba3e8d91dbf21b3f60b73c4b843
Natch <17216462+Natch@users.noreply.github.com>
2022-03-25 18:57:14 +0900

    TaskForm のデザインを反映したのでモックデザインを除去

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -15,35 +15,6 @@ export const TaskCard: VFC<Props> = ({ schedule }) => {
       <div className="pb-4">
         <TaskHeader schedule={schedule} />
       </div>
-      {/* TODO タスクリストのコンポーネントに置き換える */}
-      <ul>
-        <li>
-          <div className="flex py-2">
-            <div className="w-9 h-6 pr-3">
-              <Image
-                src="/offCheckedCheckboxIcon.svg"
-                alt="O"
-                width="24px"
-                height="24px"
-              />
-            </div>
-            <p className="text-[#757575] line-through">Next.jsのインストール</p>
-          </div>
-        </li>
-        <li>
-          <div className="flex py-2">
-            <div className="w-9 h-6 pr-3">
-              <Image
-                src="/offCheckedCheckboxIcon.svg"
-                alt="O"
-                width="24px"
-                height="24px"
-              />
-            </div>
-            <p>ESLintのインストール</p>
-          </div>
-        </li>
-      </ul>
       <TaskForm />
     </>
   );
