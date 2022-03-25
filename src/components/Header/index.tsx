38b66178ec4325c24497105baa560c2076412557
takeyu1013 <yuto1013@gmail.com>
2022-03-25 21:23:33 +0900

    🚧 ユーザアイコンを押すとプルダウンメニューのテンプレートが表示されるように変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Menu, Divider, Text } from "@mantine/core";
 
 type Props = {
   user: {
@@ -20,16 +21,39 @@ export const Header = (props: Props) => {
           />
         </a>
       </Link>
-      <Link href="#">
-        <a>
-          <Image
-            src={props.user.image}
-            alt="プロフィール画像"
-            width="36px"
-            height="36x"
-          />
-        </a>
-      </Link>
+      <Menu
+        control={
+          <button>
+            <Image
+              src={props.user.image}
+              alt="プロフィール画像"
+              width="36px"
+              height="36x"
+            />
+          </button>
+        }
+      >
+        <Menu.Label>Application</Menu.Label>
+        <Menu.Item icon={<></>}>Settings</Menu.Item>
+        <Menu.Item icon={<></>}>Messages</Menu.Item>
+        <Menu.Item icon={<></>}>Gallery</Menu.Item>
+        <Menu.Item
+          icon={<></>}
+          rightSection={
+            <Text size="xs" color="dimmed">
+              ⌘K
+            </Text>
+          }
+        >
+          Search
+        </Menu.Item>
+        <Divider />
+        <Menu.Label>Danger zone</Menu.Label>
+        <Menu.Item icon={<></>}>Transfer my data</Menu.Item>,
+        <Menu.Item color="red" icon={<></>}>
+          Delete my account
+        </Menu.Item>
+      </Menu>
     </header>
   );
 };
