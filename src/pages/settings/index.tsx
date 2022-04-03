789705f8c5976cc9f99b322f8f7837cc532f1179
takeyu1013 <yuto1013@gmail.com>
2022-04-03 17:31:44 +0900

    ✨ 閉じるアイコンを押すとタスク一覧画面に遷移する機能を追加

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from "next";
 
+import Image from "next/image";
+import Link from "next/link";
+
 const SettingsPage: NextPage = () => {
   return (
     <>
       <div className="px-48 py-7">
         <div className="flex justify-between">
-          <span className="w-6">x</span>
+          <Link href="/">
+            <a className="w-6 next-image-space-removal-wrapper">
+              <Image src="/close.svg" alt="x" width={22} height={22} />
+            </a>
+          </Link>
           <h1 className="font-bold">設定</h1>
-          <span className="w-9" />
+          <div className="w-9" />
         </div>
       </div>
     </>
