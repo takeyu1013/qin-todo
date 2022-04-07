f70cd6e23b8ffcd538e633a295695017b2465db4
takeyu1013 <yuto1013@gmail.com>
2022-04-07 22:01:58 +0900

    🔥 Mantine のスタイルをリセット

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 import Image from "next/image";
 import Link from "next/link";
-import { createStyles, Box, Text, Group } from "@mantine/core";
+import { Box, Text, Group } from "@mantine/core";
 
 const props = {
   links: [
@@ -24,60 +24,19 @@ const props = {
   ],
 };
 
-const useStyles = createStyles((theme) => ({
-  link: {
-    ...theme.fn.focusStyles(),
-    display: "block",
-    textDecoration: "none",
-    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-    lineHeight: 1.2,
-    fontSize: theme.fontSizes.md,
-    fontWeight: "bold",
-    paddingTop: theme.spacing.md,
-    paddingBottom: theme.spacing.md,
-    borderTopRightRadius: theme.radius.sm,
-    borderBottomRightRadius: theme.radius.sm,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-
-  linkActive: {
-    fontWeight: 500,
-    borderLeftColor:
-      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 6 : 7],
-    color:
-      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 2 : 7],
-
-    "&, &:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25)
-          : theme.colors[theme.primaryColor][0],
-    },
-  },
-}));
-
 interface TableOfContentsProps {
   links: { label: string; link: string; order: number }[];
   active: string;
 }
 
 export function TableOfContents({ links, active }: TableOfContentsProps) {
-  const { classes, cx } = useStyles();
   const items = links.map((item) => (
     <Box<"a">
       component="a"
       href={item.link}
       onClick={(event) => event.preventDefault()}
       key={item.label}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === item.link,
-      })}
+      className=""
       sx={(theme) => ({ paddingLeft: item.order * theme.spacing.md })}
     >
       {item.label}
