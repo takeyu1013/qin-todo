d740b5a065ce5d8cb0ddf23c748b02d6c1d57481
Natch <17216462+Natch@users.noreply.github.com>
2022-04-14 15:21:42 +0900

    認証処理を session の存在によってみなし認証とした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 import { Loader } from "../components/Loader";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
+import { Auth } from "@supabase/ui";
+import useFetchUser from "../hooks/useFetchUser";
 
 const Authentication = dynamic<AuthenticationProps>(
   () =>
@@ -15,6 +17,17 @@ const Authentication = dynamic<AuthenticationProps>(
 );
 
 const Home: NextPage = () => {
+  const { session } = Auth.useUser();
+  const { user, error, isLoading } = useFetchUser(session);
+
+  if (error) {
+    return <Loader />; // TODO: should be replaced with `Error Boundary`
+  }
+
+  if (isLoading || !user) {
+    return <Loader />;
+  }
+
   return (
     <Authentication>
       <Header
